8fd9c7609b9c6f0812bc06e479795e1697b3098d
moridodos <coderdhk06@gmail.com>
2025-05-15 14:48:16 +0900

    styles: home button + added styles directory

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import Container from 'components/Container';
+import styles from 'styles/home.module.css';
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center gap-8">
       <h1 className="text-8xl font-bold">간편한 커리어 관리</h1>
       <h3 className="text-3xl text-gray-400">내 커리어를 정리하는 가장 쉬운 방법</h3>
-      <button className="bg-[#4c8bf5] text-3xl text-white py-5 px-10 m-8 font-bold cursor-pointer hover:bg-[#3b73d1] hover:shadow-xl transition-all duration-300">
-        시작하기
-      </button>
+      <button className={`${styles.btn} text-3xl py-5 px-10 m-8`}>시작하기</button>
     </div>
   );
 }
