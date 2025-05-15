77349b4bb6b79915f0fbc21912a68f1755bb68d8
moridodos <coderdhk06@gmail.com>
2025-05-15 12:01:07 +0900

    feat: main page + tailwind에 맞는 globals.css 수정 + Container.tsx skeleton

---
+++ app/components/Container.tsx
@@ -0,0 +1,5 @@
+const Container = ({ children }: { children: React.ReactNode }) => {
+  return <div className="bg-[#2b2c31] p-[1.6rem] rounded-lg">{children}</div>;
+};
+
+export default Container;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import Container from 'components/Container';
+
 export default function Home() {
-  return <div> hello world</div>;
+  return (
+    <div className="flex flex-col items-center justify-center gap-8">
+      <h1 className="text-8xl font-bold">간편한 커리어 관리</h1>
+      <h3 className="text-3xl text-gray-400">내 커리어를 정리하는 가장 쉬운 방법</h3>
+      <button className="bg-[#4c8bf5] text-3xl text-white py-5 px-10 m-8 font-bold cursor-pointer hover:bg-[#3b73d1] hover:shadow-xl transition-all duration-300">
+        시작하기
+      </button>
+    </div>
+  );
 }
