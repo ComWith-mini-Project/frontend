980c3e83b70a366fb8c131bd6bf941669899b9db
moridodos <coderdhk06@gmail.com>
2025-06-13 15:02:06 +0900

    feat: footer link to /w/[id]

--- app/(router)/career/Footer.tsx
+++ app/(router)/career/Footer.tsx
@@ -1,22 +1,28 @@
 import clsx from 'clsx';
+import Link from 'next/link';
 
 import Globe from '@/public/globe.svg';
 import PDF from '@/public/pdf.svg';
 
 import styles from 'styles/footer.module.css';
 
-const { wrapper, inner, l_inner, button, l, r, ani } = styles;
+const { wrapper, inner, l_inner, button, l, r, web, pdf, ani } = styles;
 
 export default function Footer() {
+  // random id for the profile link
+  // This is a placeholder and should be replaced with actual logic to generate or retrieve a profile ID.
+  const randomId = Math.floor(Math.random() * 1000);
+  const id = randomId.toString();
+
   return (
     <footer className={wrapper}>
       <div className={inner}>
         <div className={l_inner}>
-          <button className={clsx(button, l, ani)}>
+          <Link href={`/w/${id}`} className={clsx(button, l, web, ani)}>
             <Globe width="2.5rem" />
             <span>내 커리어 프로필</span>
-          </button>
-          <button className={clsx(button, l, ani)}>
+          </Link>
+          <button className={clsx(button, l, pdf, ani)}>
             <PDF width="2.5rem" />
             <span>PDF로 저장</span>
           </button>
--- app/(router)/w/[id]/page.tsx
+++ app/(router)/w/[id]/page.tsx
@@ -1,11 +1,22 @@
-import { useRouter } from 'next/router';
+import Link from 'next/link';
 
-export default function Web() {
-  const router = useRouter();
-  const { id } = router.query;
+export default async function Web({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
 
   return (
     <div>
+      <Link
+        href={'/career/profile'}
+        className="text-blue-500 hover:underline mb-4"
+        style={{ fontSize: '3rem' }}
+        title="Go back to profile page"
+        aria-label="Go back to profile page"
+        tabIndex={0}
+        role="link"
+        rel="noopener"
+      >
+        go back
+      </Link>
       <h1 style={{ fontSize: '3rem' }}>Web Page {id}</h1>
       <p>This is the web page content.</p>
     </div>
